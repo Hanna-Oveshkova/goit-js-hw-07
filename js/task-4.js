07437dc07ed9b0eb403bae3cce2d76445aaa5c32
Ganusya <ganusya.ua@gmail.com>
2026-02-19 21:44:48 +0200

    add files

--- js/task-4.js
+++ js/task-4.js
@@ -1,18 +1,17 @@
 const form = document.querySelector(".login - form");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  const { email, password } = form.elements;
+  const { email, password } = loginForm.elements;
   const emailValue = email.value.trim();
   const passwordValue = password.value.trim();
-  if (emailValue === "" || passwordValue === "") {
+  if (!emailValue || !passwordValue) {
     alert("All form fields must be filled in");
     return;
   }
-    const data = {
-        email = emailValue,
-        password = passwordValue,
-    };
-    console.log(data);
-    form.reset();
-    
+  const formData = {
+    email: emailValue,
+    password: passwordValue,
+  };
+  console.log(formData);
+  form.reset();
 });
--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,10 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const btnChangeColor = document.querySelector("button.change-color");
+const spanColor = document.querySelector("span.color");
+btnChangeColor.addEventListener("click", () => {
+  const color = getRandomHexColor(); // генеруємо новий колір
+  document.body.style.backgroundColor = color; // змінюємо фон через інлайн-стиль
+  spanColor.textContent = color; // записуємо hex-код у span
+});
