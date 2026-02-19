0912b78878520474406e96f57b60e44f2a5364aa
Ganusya <ganusya.ua@gmail.com>
2026-02-19 21:58:05 +0200

    fix task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,7 +1,7 @@
-const form = document.querySelector(".login - form");
+const form = document.querySelector(".login-form");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  const { email, password } = loginForm.elements;
+  const { email, password } = form.elements;
   const emailValue = email.value.trim();
   const passwordValue = password.value.trim();
   if (!emailValue || !passwordValue) {
